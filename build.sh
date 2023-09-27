d6222622bc0b37dcc959ce1f01917836f0b7e0cb
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-09-27 15:03:56 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+dd if=/dev/zero of=linuxroot1.img bs=1M count=6800
+mkfs.ext4 linuxroot1.img
+mkdir temp1 && sudo mount linuxroot1.img ./temp1
+sudo cp -rfp rootfs/*  temp1/
+sudo umount temp1/
+e2fsck -p -f linuxroot1.img
+resize2fs  -M linuxroot1.img
