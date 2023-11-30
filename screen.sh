7b00eeda3fc244929e31ee9d75dcf4aa434dace5
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-11-30 11:10:45 +0800

    Create screen.sh

---
+++ screen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ 1 -eq `xrandr | grep connected | wc -l` ]; then
+	return
+fi
+path=/etc/profile.d/res.sh
+if [ ! -f $path ]; then
+	touch $path
+fi
+if [ -O $path ]; then
+	device='HDMI-1'
+	info='"800x480 60.0"'
+	result=`cvt 800 480 | awk -v FS='Modeline' '{print $2}'`
+	echo '#!/bin/sh' > $path
+	echo xrandr --newmode $result >> $path
+	echo xrandr --addmode $device $info >> $path
+	echo xrandr --output $device --mode $info >> $path
+fi
