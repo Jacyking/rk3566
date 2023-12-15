06e63a6cd1e19ce5804390a645ed465175f70216
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-12-15 11:37:03 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 dirname=`dirname $0`
+
+if [ -f "$dirname/s7server2" ]; then
+        sudo mv $dirname/s7server2 $dirname/s7server
+fi
+
 if [ 0 -eq `ps -elf | grep -w s7server | grep -v grep | wc -l` ]; then
         sudo $dirname/s7server /dev/ttyS9 > $dirname/output.log 2>&1
 fi
