bc7522e80193d2649465948954053dc527c8f48a
Jacyking <43704572+Jacyking@users.noreply.github.com>
2023-12-18 10:51:46 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,6 +5,10 @@ if [ -f "$dirname/s7server2" ]; then
         sudo mv $dirname/s7server2 $dirname/s7server
 fi
 
+if [ ! -x "$dirname/s7server" ]; then
+        sudo chmod +x $dirname/s7server
+fi
+
 if [ 0 -eq `ps -elf | grep -w s7server | grep -v grep | wc -l` ]; then
         sudo $dirname/s7server /dev/ttyS9 > $dirname/output.log 2>&1
 fi
